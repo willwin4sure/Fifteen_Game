870028653cf52a2f445ff07244de0a78eee36b5d
William Yue <williamy330@gmail.com>
2023-08-27 10:35:02 -0400

    added Tiles

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include "src/Tile.h"
+
 int main()
 {
-    std::cout << "Hello, world!\n";
+    Tile tile1{ 10 };
+    Tile tile2{ 8 };
+    Tile tile3{ 0 }; // the missing tile
+    Tile tile4{ 1 };
+
+    std::cout << "0123456789ABCDEF\n"; // to make it easy to see how many spaces are in the next line
+    std::cout << tile1 << tile2 << tile3 << tile4 << '\n';
+
+    std::cout << std::boolalpha << tile1.isEmpty() << ' ' << tile3.isEmpty() << '\n';
+    std::cout << "Tile 2 has number: " << tile2.getNum() << "\nTile 4 has number: " << tile4.getNum() << '\n';
 
     return 0;
 }
